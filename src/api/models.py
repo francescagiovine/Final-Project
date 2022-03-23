32e6e8d29cadc473e63f54ac261962e4a40ecb60
Jose <josejesuscedeno@gmail.com>
2022-03-23 09:31:27 +0000

    estoy creando los otros modelos

--- src/api/models.py
+++ src/api/models.py
@@ -10,7 +10,13 @@ class User(db.Model):
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
-
+class Travel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    Location = db.Column(db.varchar(), unique=False)
+    begin_date = db.Column(db.Data(), unique=False)
+    end_date = db.Column(db.Data(), unique=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
--- src/api/routes.py
+++ src/api/routes.py
@@ -16,3 +16,27 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+#api 1 - login, here i create the login service
+@api.route('/login', methods=['POST'])
+def login():
+    #here I take the data from the api for login
+    email = request.json.get('email') 
+    password = request.json.get('password') 
+
+    user = User.query.filter_by(email=email, password=password).first()
+    if not user:
+        return jsonify({"message":"El usuario o contraseña incorrectos"}), 401
+
+    token = create_access_token(identity=user.id)
+
+    data_response = { #here I create a dictionary and I put de data from the API
+        "token" : token,
+        "email": user.email,
+        "user_id": user.id
+    }
+
+    return jsonify(data_response), 200 # here I send de data stored in the dictionary as a response
+
+# this way we are sure the data is coming from the api and is the right data
+# end of api 1 - login
