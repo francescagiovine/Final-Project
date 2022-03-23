ee4e132a5a06bb83f35cde0f45c0496a660ad19e
pereiraalvaro14 <pereiraalvaro@gmail.com>
2022-03-23 19:04:24 +0000

    Formulario Sign Up adelantado

--- src/api/routes.py
+++ src/api/routes.py
@@ -16,3 +16,12 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/signUp', methods=['POST'])
+def handle_signUp():
+
+    response_body = {
+        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+    }
+
+    return jsonify(response_body), 200
