5930f06dd4683ad8a0d8a302789c14014d89ec10
Jose <josejesuscedeno@gmail.com>
2022-03-27 12:22:48 +0000

    listo el servicio de api para el login

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,7 +4,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
-from flask_jwt_extended import jwt_required, create_access_token
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token
 
 api = Blueprint('api', __name__)
 
@@ -19,31 +19,28 @@ def handle_hello():
     return jsonify(response_body), 200
 
 #api 1 - login, here i create the login service
-@api.route('/login', methods=['POST', 'GET'])
+@api.route('/login', methods=['POST'])
 def login():
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+    email = request.json.get('email') 
+    password = request.json.get('password') 
+
+    user = User.query.filter_by(email=email, password=password).first()
+    if not user:
+        return jsonify({"message":"El usuario o contraseña incorrectos"}), 401
+
+    token = create_access_token(identity = user.id)
+
+    data_response = {
+        "token" : token,
+        "email": email,
+        "password": password
     }
 
-    return jsonify(response_body), 200
-    #here I take the data from the api for login
-    #email = request.json.get('email') 
-    #password = request.json.get('password') 
+    return jsonify(data_response), 200
 
-    #user = User.query.filter_by(email=email, password=password).first()
-    #if not user:
-    #    return jsonify({"message":"El usuario o contraseña incorrectos"}), 401
 
-    #token = create_access_token(identity=user.id)
-
-    #data_response = { #here I create a dictionary and I put de data from the API
-    #    "token" : token,
-    #    "email": user.email,
-    #    "user_id": user.id
-    #}
-
-    #return jsonify(data_response), 200 # here I send de data stored in the dictionary as a response
+ 
 
 # this way we are sure the data is coming from the api and is the right data
 # end of api 1 - login
