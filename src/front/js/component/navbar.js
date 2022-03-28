3e32f3362313acc805d8180c07aa9393262ae5b9
francescagiovine <francescagiovine@hotmail.com>
2022-03-28 15:08:53 +0000

    first lading

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -2,18 +2,19 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <Link to="/home" className="buttons">
+          WideTravel
+        </Link>
+        <Link to="/">
+          <span className="navbar-brand">WideTravel</span>
+          <button className="buttons">Nosotros</button>
+          <button className="buttons">Reseñas</button>
+          <button className="login">LOG IN</button>
+        </Link>
+      </div>
+    </nav>
+  );
 };
