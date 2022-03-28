fa253a280eb3ecd34885e882b7d8ad25e9af9930
Jose <josejesuscedeno@gmail.com>
2022-03-28 16:11:24 +0000

    Creado el formulario, ahora viene el post

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -8,7 +8,24 @@ export const Login = () => {
 
 	return (
 		<div className="container">
-            <h1>Aquí es donde irá el formulario para el login</h1>
+            <h1>Login</h1>
+			<form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Your Email</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1"/>
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
+    <label class="form-check-label" for="exampleCheck1">Remember me</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Login</button>
+</form>
+			
 			
 			<br />
 			<Link to="/">
