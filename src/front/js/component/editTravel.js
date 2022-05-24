183e0249f1e0251d962e9534377ddd2906fb729a
pereiraalvaro14 <pereiraalvaro@gmail.com>
2022-05-24 14:38:25 +0000

    Ultimos cambios, agregado validacion de fechas y pequeños cambios en diseño formulario

--- src/front/js/component/editTravel.js
+++ src/front/js/component/editTravel.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import "../../styles/home.css";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
+import moment from "moment";
 
 export default function EditTravel() {
   const { id } = useParams();
@@ -36,7 +37,6 @@ export default function EditTravel() {
   const [submitted, setSubmitted] = useState(false);
   const [error, setError] = useState(false);
 
-
   // Handling the name change
   const handleName = (e) => {
     console.log(e.target.value);
@@ -56,7 +56,6 @@ export default function EditTravel() {
     setDescription(e.target.value);
     setSubmitted(false);
   };
-
 
   // Handling the beginDate change
   const handleBeginDate = (e) => {
@@ -86,34 +85,43 @@ export default function EditTravel() {
       end_date === ""
     ) {
       setError("Please enter all the fields");
-    } else {
-      fetch(process.env.BACKEND_URL + "/api/editTravel", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: "Bearer " + token,
-        },
-        body: JSON.stringify({
-          id: id,
-          name: name,
-          location: location,
-          description: description,
-          begin_date: begin_date,
-          end_date: end_date,
-          latitude: locationUrl,
-        }),
-      })
-        .then((resp) => resp.json())
-        .then((data) => {
-          console.log("editTravel");
-          setSubmitted(true);
-          setError(false);
-        })
-        .catch((error) => {
-          // setError(error);
-          console.log(error);
-        });
+      return;
     }
+
+    const beginDateMoment = moment(begin_date);
+    const endDateMoment = moment(end_date);
+
+    if (beginDateMoment.isAfter(endDateMoment)) {
+      setError("Begin date must be before end date");
+      return;
+    }
+
+    fetch(process.env.BACKEND_URL + "/api/editTravel", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
+      body: JSON.stringify({
+        id: id,
+        name: name,
+        location: location,
+        description: description,
+        begin_date: begin_date,
+        end_date: end_date,
+        latitude: locationUrl,
+      }),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log("editTravel");
+        setSubmitted(true);
+        setError(false);
+      })
+      .catch((error) => {
+        // setError(error);
+        console.log(error);
+      });
   };
 
   // Showing success message
@@ -125,12 +133,12 @@ export default function EditTravel() {
           display: submitted ? "" : "none",
         }}
       >
-        <h1 className="corporative">
+        <h3 className="corporative">
           You edited your travel to {name} successfully
-        </h1>
-        <h1 className="corporative">
+        </h3>
+        <h3 className="corporative">
           <Link to="/private">Back to Travels</Link>
-        </h1>
+        </h3>
       </div>
     );
   };
@@ -144,16 +152,16 @@ export default function EditTravel() {
           display: error != false ? "" : "none",
         }}
       >
-        <h1 className="corporative">{error}</h1>
+        <h3 className="corporative">{error}</h3>
       </div>
     );
   };
 
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col">
-          <div className="App form pt-2 pb-2 rounded">
+    <form>
+      <div className="container App form pt-2 pb-2 rounded">
+        <div className="row">
+          <div className="col-12">
             <div>
               <h1 className="title">Edit your travel</h1>
             </div>
@@ -163,64 +171,65 @@ export default function EditTravel() {
               {errorMessage()}
               {successMessage()}
             </div>
+          </div>
+          <div className="col-md-6">
+            {/* Labels and inputs for form data */}
+            <label className="label">Name</label>
+            <input
+              onChange={handleName}
+              className="input"
+              defaultValue={name}
+              type="text"
+            />
 
-            <form>
-              {/* Labels and inputs for form data */}
-              <label className="label">Name</label>
-              <input
-                onChange={handleName}
-                className="input"
-                defaultValue={name}
-                type="text"
-              />
+            <label className="label">Begin Date</label>
+            <input
+              defaultValue={begin_date}
+              onChange={handleBeginDate}
+              className="input"
+              type="datetime-local"
+            />
 
-              <label className="label">Location</label>
-              <input
-                onChange={handleLocation}
-                className="input"
-                value={location}
-                type="text"
-              />
+            <label className="label">Description</label>
+            <textarea
+              onChange={handleDescription}
+              className="input"
+              value={description}
+              type="text"
+            />
+          </div>
+          <div className="col-md-6">
+            <label className="label">Location</label>
+            <input
+              onChange={handleLocation}
+              className="input"
+              value={location}
+              type="text"
+            />
 
-              <label className="label">Description</label>
-              <textarea
-                onChange={handleDescription}
-                className="input"
-                value={description}
-                type="text"
-              />
+            <label className="label">End Date</label>
+            <input
+              type="datetime-local"
+              className="input"
+              onChange={handleEndDate}
+              defaultValue={end_date}
+            />
 
-              <label className="label">Begin Date</label>
-              <input
-                defaultValue={begin_date}
-                onChange={handleBeginDate}
-                className="input"
-                type="datetime-local"
-              />
-
-              <label className="label">End Date</label>
-              <input
-                type="datetime-local"
-                className="input"
-                onChange={handleEndDate}
-                defaultValue={end_date}
-              />
-
-              <label className="label">URL</label>
-              <input
-                onChange={handleLocationUrl}
-                className="input"
-                defaultValue={locationUrl}
-                type="text"
-              />
-
-              <button onClick={handleSubmit} className="btn btn-user">
-                Save
-              </button>
-            </form>
+            <label className="label">URL</label>
+            <input
+              onChange={handleLocationUrl}
+              className="input"
+              defaultValue={locationUrl}
+              type="text"
+            />
+          </div>
+          <div className="col-12">
+            <button onClick={handleSubmit} className="btn btn-user">
+              Save
+            </button>
           </div>
         </div>
       </div>
-    </div>
+    </form>
   );
 }
